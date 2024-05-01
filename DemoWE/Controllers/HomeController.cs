636d7dddde034263f7c6a421aba873b3df2d1be8
saFaisal <sa.faisal.ksa@gmail.com>
2024-05-01 15:31:53 +0300

    Home Page

--- DemoWE/Controllers/HomeController.cs
+++ DemoWE/Controllers/HomeController.cs
@@ -20,11 +20,21 @@ namespace DemoWE.Controllers
             _context = context;
         }
 
-        public IActionResult Employee()
+        public async Task<IActionResult> Employee(string? DepartmentID)
         {
-            
-            string username = HttpContext.Session.GetString("Username");
+
+            string userId = HttpContext.Session.GetString("userid");
+            int userIdInt = Convert.ToInt32(userId);
+            string username = HttpContext.Session.GetString("Username") ?? string.Empty;
             ViewBag.name = username;
+            string DeptID = HttpContext.Session.GetString("DepartmentID") ?? string.Empty;
+            ViewBag.Department = DeptID;
+            string DepartmentName = string.Empty; // Initialize DepartmentName variable
+
+            Department departmentName = await _context.Department.FirstOrDefaultAsync(t => t.DepartmentID.ToString() == DeptID);
+            DepartmentName = departmentName?.DepartmentName ?? string.Empty;
+
+            ViewBag.DepartmentName = DepartmentName; // Assign DepartmentName to ViewBag
 
             return View();
         }
@@ -41,7 +51,6 @@ namespace DemoWE.Controllers
         }
 
         [HttpPost]
-
         public List<object> GetData(int? AssignedTo, int? CreatedBy)
         {
 
@@ -49,16 +58,61 @@ namespace DemoWE.Controllers
             int userIdInt = Convert.ToInt32(userId);
             List<object> Data = new List<object>();
 
-            
-                List<string> labels = _context.STask.Where(x => x.AssignedTo == userIdInt).Select(x => x.TaskTitle).ToList();
-                Data.Add(labels);
-                List<DateTime> StartDate = _context.STask.Where(x => x.AssignedTo == userIdInt).Select(x => x.StartDate).ToList();
-                Data.Add(StartDate);
-                List<DateTime> Deadline = _context.STask.Where(x => x.AssignedTo == userIdInt).Select(x => x.Deadline).ToList();
-                Data.Add(Deadline);
-            
+            List<string> labels = _context.STask.Where(x => x.AssignedTo == userIdInt).Select(x => x.TaskTitle).ToList();
+            Data.Add(labels);
+            List<DateTime> StartDate = _context.STask.Where(x => x.AssignedTo == userIdInt).Select(x => x.StartDate).ToList();
+            Data.Add(StartDate);
+            List<DateTime> Deadline = _context.STask.Where(x => x.AssignedTo == userIdInt).Select(x => x.Deadline).ToList();
+            Data.Add(Deadline);
 
             return Data;
+        }
+
+        public List<object> TData(int? AssignedTo, int? CreatedBy)
+        {
+            string userId = HttpContext.Session.GetString("userid");
+            int userIdInt = Convert.ToInt32(userId);
+
+            List<object> SData = new List<object>();
+
+            List<string> TaskTitle = _context.STask.Where(x => x.AssignedTo == userIdInt)
+                                                   .OrderByDescending(x => x.TaskID)
+                                                   .Take(3)
+                                                   .Select(x => x.TaskTitle)
+                                                   .ToList();
+            List<Status> status = _context.STask.Where(x => x.AssignedTo == userIdInt)
+                                                .OrderByDescending(x => x.TaskID)
+                                                .Take(3)
+                                                .Select(x => x.Status)
+                                                .ToList();
+
+            List<string> statusText = new List<string>();
+            foreach (var s in status)
+            {
+                switch (s)
+                {
+                    case Status.New:
+                        statusText.Add("New");
+                        break;
+                    case Status.InProgress:
+                        statusText.Add("InProgress");
+                        break;
+                    case Status.Declined:
+                        statusText.Add("Declined");
+                        break;
+                    case Status.Completed:
+                        statusText.Add("Completed");
+                        break;
+                    default:
+                        statusText.Add("Unknown");
+                        break;
+                }
+            }
+
+            SData.Add(TaskTitle);
+            SData.Add(statusText);
+
+            return SData;
         }
     }
 }
